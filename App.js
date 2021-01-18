3b9499295f481a41009a1af58027d86d5ffe03e5
Laura Hénocq <laura.henocq@gmail.com>
2021-01-18 10:39:15 +0100

    Initialisation du projet clean 3000

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { LogoTitle } from './src/LogoTitle';
+import { Home } from './src/Home';
+import { Notice } from './src/Notice';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        screenOptions={{
+          title: 'Clean 3000',
+          headerStyle: {
+              backgroundColor: '#3d3d33',
+              height:50
+          },
+          headerTintColor: '#5cba47',
+          headerTitleStyle: {
+              fontWeight: 'bold',
+          },
+          headerTitleAlign:"center"
+        }}>
+
+          <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            headerTitle: () => <LogoTitle/>
+          }}
+          />
+
+          <Stack.Screen 
+          name="Notice" 
+          component={Notice}
+          options={{
+            headerTitle: 'Avis de passage',
+            headerBackTitle:"Acceuil"
+          }}
+          />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ customers.js
@@ -0,0 +1,44 @@
+const customers = [
+    {
+        id:"C-001",
+        name: "Jardins d'Ariana",
+        localisation:"Résidence les Jardins d'Ariana59280 Armentières",
+        image: "",
+        description: "Résidence",
+        color: "#B2160A"
+    },
+    {
+        id:"C-002",
+        name: "Jérôme Livran",
+        localisation:"215 Avenue le Notre, 59100 Roubaix",
+        image: "",
+        description: "Nortia",
+        color: "#BB9E31"
+    },
+    {
+        id:"C-003",
+        name: "Philippe Parguey",
+        localisation:"215 Avenue le Notre, 59100 Roubaix",
+        image: "",
+        description: "Nortia Invest",
+        color: "#392A15"
+    },
+    {
+        id:"C-004",
+        name: "Archimed",
+        localisation:"49 Boulevard de Strasbourg, 59000 Lille",
+        image: "http://api.elorri.fr/coffee-app/3700110005397.png",
+        description: "Entreprise de logiciels",
+        color: "#F77318"
+    },
+    {
+        id:"C-005",
+        name: "BeCom",
+        localisation:"40 Rue du Maréchal Foch, 59100 Roubaix",
+        image: "",
+        description: "Agence de communication et création de sites Web",
+        color: "#392A15"
+    }
+];
+
+export default customers;
